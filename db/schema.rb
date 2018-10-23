52bf7084122fa0861089c18b117b227976b48b41
vagrant <vagrant@vagrant.vm>
2018-10-23 19:30:23 +0000

    feat:Catalogo productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_190252) do
+ActiveRecord::Schema.define(version: 2018_10_23_192413) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "rfc"
     t.string "nombre_cliente"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.string "nombre_producto"
+    t.float "precio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
